a277da26a84892fa23b413fbceb117ef92cac06d
Tim Diggins <tim@red56.uk>
2023-11-27 16:59:00 +0000

    Squashed commit of the following:
    commit 311250b73a9b318ad59e91e99eb85bfa27491ff0
    Author: Tim Diggins <tim@red56.uk>
    Date:   Mon Nov 27 16:52:01 2023 +0000
    
        optional refactoring to make intention more crisp
    
    commit 65b93a2f4e8eba2cb2cf8092e7e81b630b295730
    Author: Tim Diggins <tim@red56.uk>
    Date:   Mon Nov 27 16:43:18 2023 +0000
    
        rename method to more accurately reflect action
    
    commit d9f9dbdc36494c159109fd4d51d0eaea43ce4bc4
    Author: Tim Diggins <tim@red56.uk>
    Date:   Mon Nov 27 16:38:22 2023 +0000
    
        allow s3_object put to specify metadata again
    
        fix erroneous change to meaning
    
        fixes #1881

--- plugins/modules/s3_object.py
+++ plugins/modules/s3_object.py
@@ -654,15 +654,9 @@ def path_check(path):
         return False
 
 
-def get_content_type(src, present=True):
-    if not present:
-        content_type = None
-        if src:
-            content_type = mimetypes.guess_type(src)[0]
-        if content_type is None:
-            # s3 default content type
-            content_type = "binary/octet-stream"
-        return content_type
+def guess_content_type(src):
+    content_type = mimetypes.guess_type(src)[0] if src else None
+    return content_type if content_type else "binary/octet-stream"
 
 
 def get_extra_params(
@@ -747,7 +741,8 @@ def upload_s3file(
             elif isinstance(permissions, list):
                 extra["ACL"] = permissions[0]
 
-        extra["ContentType"] = get_content_type(src, present=extra.get("ContentType"))
+        if "ContentType" not in extra:
+            extra["ContentType"] = guess_content_type(src)
 
         if src:
             s3.upload_file(aws_retry=True, Filename=src, Bucket=bucket, Key=obj, ExtraArgs=extra)
